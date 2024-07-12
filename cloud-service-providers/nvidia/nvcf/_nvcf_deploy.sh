28839e6decf9392e4c98945bfa77064938da9f5c
Kyle Huang <yellowsea@gmail.com>
2024-07-11 17:31:58 -0700

    folder change

--- cloud-service-providers/nvidia/nvcf/_nvcf_deploy.sh
+++ cloud-service-providers/nvidia/nvcf/_nvcf_deploy.sh
@@ -1,3 +1,4 @@
+# Deploy the Cloud Function onto L40 GPU with min/max instance set to 1/1
 ngc cloud-function function deploy create \
     --deployment-specification GFN:L40:gl40_1.br20_2xlarge:1:1 \
     FUNCION_ID:FUNCTION_VERSION
